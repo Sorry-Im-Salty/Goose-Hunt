84080803e6ea830f59930e9ab6853e8c332f912a
Sorry-Im-Salty <47731654+Sorry-Im-Salty@users.noreply.github.com>
2019-09-03 16:28:17 +1000

    Added weaponShoot Script.

---
+++ Goose Hunt/Assets/Scripts/weaponShoot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponShoot : MonoBehaviour
+{
+	public Camera GunCamera;
+	public float range = 100.0f;
+	public ParticleSystem muzzleFlash;
+
+
+	private void Awake()
+	{
+		muzzleFlash.Stop();
+	}
+	// Update is called once per frame
+	void Update()
+    {
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Shoot();
+		}
+    }
+
+	void Shoot()
+	{
+		muzzleFlash.Play();
+		RaycastHit hit;
+		if (Physics.Raycast(GunCamera.transform.position, GunCamera.transform.forward, out hit, range))
+		{
+			Debug.Log(hit.transform.name);
+		}
+	}
+}
