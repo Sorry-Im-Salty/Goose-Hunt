df4dd8a8cb3d5aac253fe33c0a4d5dd380e60720
Connor L <47731615+SomethingAround@users.noreply.github.com>
2019-09-09 14:02:56 +1000

    Added Prefabs Folder Added Cube(Goose) Prefab Added Spawner Prefab Added Goose Script Added Spawner Script

---
+++ Goose Hunt/Assets/Scripts/Goose.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Created by Connor Li
+public class Goose : MonoBehaviour
+{
+	float m_timer;
+	public float m_secondsActive;
+	public float m_xVelocityMin = 10.0f;
+	public float m_xVelocityMax = 20.0f;
+	public float m_yVelocity = 20.0f;
+	bool MouseOver = false;
+	Transform m_Spawner;
+	Collider m_Collider;
+	Rigidbody m_rb;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+		m_Spawner = GameObject.FindGameObjectWithTag("Spawner").GetComponent<Transform>();
+		m_rb = gameObject.GetComponent<Rigidbody>();
+		m_Collider = gameObject.GetComponent<Collider>();
+		m_rb.velocity = new Vector3(Random.Range(m_xVelocityMin, m_xVelocityMax), m_yVelocity, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		m_timer += Time.deltaTime;
+		//Resets the goose
+		if (m_timer >= 5.0f)
+		{
+			gameObject.SetActive(false);
+			gameObject.transform.position = GetComponentInParent<Transform>().position;
+			m_rb.velocity = new Vector3(Random.Range(m_xVelocityMin, m_xVelocityMax), m_yVelocity, 0.0f);
+			m_timer = 0.0f;
+		}
+    }
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		//Checks if the collision is with another goose
+		if (collision.gameObject.tag == "Goose")
+		{
+			Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), m_Collider);
+		}
+		//Checks if the collision is with the ground
+		else if(collision.gameObject.tag == "Ground")
+		{
+			Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), m_Collider);
+		}
+		//Checks if the collision is with the Spawner
+		else if (collision.gameObject.tag == "Spawner")
+		{
+			Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), m_Collider);
+		}
+		//Checks if the collision is with the bullet
+		if (collision.gameObject.tag == "Bullet")
+		{
+			m_rb.useGravity = true;
+		}
+	}
+}
---
+++ Goose Hunt/Assets/Scripts/Spawner.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Created by Connor Li
+public class Spawner : MonoBehaviour
+{
+	int m_goose;
+	float m_timer;
+	public float m_spawnTimer = 5.0f;
+	public static int m_objects = 10;
+	public Object m_fly;
+	GameObject[] m_ob = new GameObject[m_objects];
+	public Transform m_parent;
+	bool MouseOver = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+		//Creates Goose Spawn Pool 
+		for (int i = 0; i < m_objects; ++i)
+		{
+			m_ob[i] = Instantiate(m_fly, m_parent) as GameObject;
+			m_ob[i].SetActive(false);
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		m_timer += Time.deltaTime;
+		//Spawn
+		if (m_timer >= m_spawnTimer)
+		{
+			//Checks the amount of objects
+			if(m_goose == m_objects)
+			{
+				m_goose = 0;
+			}
+			//Checks if the object is inactive
+			if (m_ob[m_goose].activeInHierarchy == false)
+			{
+				m_ob[m_goose].SetActive(true);
+				++m_goose;
+			}
+
+			m_timer = 0;
+		}
+    }
+
+	void OnMouseOver()
+	{
+		MouseOver = true;
+	}
+	void OnMouseExit()
+	{
+		MouseOver = false;
+	}
+}
