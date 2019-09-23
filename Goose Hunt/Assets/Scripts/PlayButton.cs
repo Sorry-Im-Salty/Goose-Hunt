499c6dbd4470728b82b2d2e23fdb79a58eb015a3
GrantRoberts <47731580+GrantRoberts@users.noreply.github.com>
2019-09-23 10:29:38 +1000

    Added PlayButton script Added Green material Added the main game scene

---
+++ Goose Hunt/Assets/Scripts/PlayButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// The PlayButton
+// Created by Grant Roberts
+// Move on to the next scene.
+
+public class PlayButton : MonoBehaviour
+{
+	private void OnCollisionEnter(Collision collision)
+	{
+#if (UNITY_ANDROID)
+		if (collision.gameObject.tag == "Bullet")
+			SceneManager.LoadScene(1);
+#endif
+	}
+}
